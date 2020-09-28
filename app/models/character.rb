d9df8384ca16b9acd2cb9c01aa3dbe7841cf9bd5
Matthew Kirby <matt.kirby@yahoo.com>
2020-09-28 15:52:50 -0400

    added the Character class associations

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,7 @@
 class Character < ActiveRecord::Base
-
-
+    belongs_to :user
+    has_many :save_tables
+    has_many :items, through: :save_tables
+    has_many :escapes, through: :save_tables
+    
 end
