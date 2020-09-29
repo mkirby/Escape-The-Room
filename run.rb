ca20315a4f6d3302e3316720acbc9feff4edede7
Matthew Kirby <matt.kirby@yahoo.com>
2020-09-29 12:25:09 -0400

    updated user class and fixed environment bug

--- run.rb
+++ run.rb
@@ -1,3 +1,5 @@
+require 'pry'
+require_relative './config/environment'
 require "tty-prompt"
 
 puts "
@@ -13,22 +15,4 @@ puts "\nA SPOOKY- HORROR ADVENTURE APP WHERE YOU ESCAPE THE ROOM OR DIE!\n\n\n"
 puts "BEGIN"
 gets.chomp
 
-def main_menu
-    prompt = TTY::Prompt.new
-    system("clear")
-    welcome = prompt.select("") do |menu|
-        menu.choice "Sign Up"
-        menu.choice "Log In"
-    end
-    if welcome == "Log In"
-        # puts "on your way to login"
-        #to log in method
-        #User.log_in
-    elsif welcome == "Sign Up"
-        # puts "on your way to signup"
-        #to sign up method
-        #User.sign_up
-    end
-end
-
-main_menu
+User.main_menu
