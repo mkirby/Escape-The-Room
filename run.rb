397f9bd39b66c30fe24361a4792516c71f39672d
Matthew Kirby <matt.kirby@yahoo.com>
2020-09-30 12:33:47 -0400

    created EscapeTheRoom class to store session variables

--- run.rb
+++ run.rb
@@ -1,15 +1,31 @@
 require 'pry'
 require_relative './config/environment'
 require "tty-prompt"
-prompt = TTY::Prompt.new
 
-#Logo.run
+class EscapeTheRoom
+    attr_accessor :session_user, :session_character
 
-puts "\n\n\nA SPOOKY - HORROR ADVENTURE APP WHERE YOU ESCAPE THE ROOM OR DIE!\n\n\n"
-sleep 1
-prompt.keypress("Press space or enter to begin", keys: [:space, :return])
+    def initialize(session_user, session_character = nil)
+        @session_user = session_user
+        @session_character = session_character
+    end
 
-$logged_in_user = nil
-$current_character = nil
-system("clear")
-User.main_menu
+    def self.run
+        prompt = TTY::Prompt.new
+        system("clear")
+        puts "[LOGO HERE]"
+        #Logo.run
+        puts "\n\n\nA SPOOKY - HORROR ADVENTURE APP WHERE YOU ESCAPE THE ROOM OR DIE!"
+        sleep 1
+        prompt.keypress("Press space or enter to begin", keys: [:space, :return])
+
+        #to be removed once changed in other User methods
+        $logged_in_user = nil
+        $current_character = nil
+
+        system("clear")
+        User.main_menu
+    end
+end
+
+EscapeTheRoom.run
