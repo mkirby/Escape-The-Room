78c11c654df8702ad7e4f4020574e0e8281ae12c
Matthew Kirby <matt.kirby@yahoo.com>
2020-09-30 10:27:45 -0400

    added Character#view_stats

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,4 +4,11 @@ class Character < ActiveRecord::Base
     has_many :items, through: :records
     has_many :escapes, through: :records
 
+    def check_stats
+        puts "Name: #{self.name}"
+        puts "Health: #{self.health}"
+        puts "Terror: #{self.terror}"
+    end
+
+
 end
