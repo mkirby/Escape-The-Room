98d8db09dd40a79a2ef678a526b14e2ebe7b8c90
Matthew Kirby <matt.kirby@yahoo.com>
2020-10-01 09:57:04 -0400

    added character health, terror, and picking up item changes to escape class

--- app/models/escape.rb
+++ app/models/escape.rb
@@ -88,15 +88,16 @@ class Escape < ActiveRecord::Base
             self.cage  ##Sends back to start of cage story of waking up dizzy
             
         elsif choice == "Reach for cue stick"
-            ###Need story of character grabbing stick and add cue stick to character.items
-            #puts "You now have a cue stick, what what you like to do with it?"
+            #remove the add item
+            Item.create(name: "Cue Stick", description: "It's a cue stick")
+            EscapeTheRoom.add_character_item("Cue Stick")
             choice2 = prompt.select('You now have a cue stick, what what you like to do with it?') do |menu|
                 menu.choice "Rattle cage and scream for help"
                 menu.choice "Reach for keys with cue stick"
             end
             if choice2 == "Rattle cage and scream for help"
                 puts "You rattle the cage and yell for help\n\nA man runs down the stairs towards your cage.\n\nHe opens your cage door...unfortunately he has a bat.\n\nThat's the last thing you see before being knocked out.\n\n\n"
-                ###character.health =-3
+                EscapeTheRoom.change_health(-3)
                 sleep 3
                 prompt.keypress("Press space or enter to try again", keys: [:space, :return])
                 system("clear")
@@ -104,7 +105,9 @@ class Escape < ActiveRecord::Base
 
             elsif choice2 == "Reach for keys with cue stick"
                 puts "You reach for the keys with the cue stick\n\nMiraculously you are about to get the key ring to slide down\n\nthe cue stick!! You pull the cue stick back into the cage\n\nand grab the keys! FREEDOM!!!"
-                ###character.item ADD key and cuestick
+                #remove the add item
+                Item.create(name: "Keys", description: "It's keys")
+                EscapeTheRoom.add_character_item("Keys")
                 sleep 3
                 choice3 = prompt.select('You now have the keys, what what you like to do with it?') do |menu|
                     menu.choice "Unlock the cage"
@@ -118,7 +121,7 @@ class Escape < ActiveRecord::Base
         elsif choice == "Reach for keys"
             ###Need story of character grabbing keys immediate and hurting arm decreasing health by 1
             puts "You extend your arms and reach out for the keys\n\nyour fingertips are so close you can feel the chill from the metal.\n\nAHHH!! A bat flies in and tries to perch on your extended arm!\n\nThat's the last thing you remember before fainting from fear\n\n\n"
-            ###character.terror =+3
+            EscapeTheRoom.change_terror(3)
             sleep 3
             prompt.keypress("Press space or enter to try again", keys: [:space, :return])
             system("clear")
