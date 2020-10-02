c619b100e70932bfc89aa819a86e2fb91e3c3b25
Matthew Kirby <matt.kirby@yahoo.com>
2020-10-01 22:23:27 -0400

    added the logo back in the cleaned up the run file

--- run.rb
+++ run.rb
@@ -5,11 +5,8 @@ prompt = TTY::Prompt.new
 
 
 system("clear")
-puts "[LOGO HERE]"
-#Logo.run
-puts "\n\n\nA SPOOKY - HORROR ADVENTURE APP WHERE YOU ESCAPE THE ROOM OR DIE!"
-sleep 1
-prompt.keypress("Press space or enter to begin", keys: [:space, :return])
+Logo.run
+puts "\n\n\nA SPOOKY - HORROR ADVENTURE APP WHERE YOU ESCAPE THE ROOM OR DIE!\n\n"
+sleep 2
 
-system("clear")
 EscapeTheRoom.main_menu
